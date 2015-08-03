2c3f10e069d7e3ace50a82429d3219878d893ef4
Jacob Mathison <jmathison@bitbasic.com>
2015-08-03 09:28:55 -0500

    Example fix

--- tests/example.php
+++ tests/example.php
@@ -1,17 +1,17 @@
 <?php
-	require_once(__DIR__ . '/../vendor/autoload.php');
+    require_once(__DIR__ . '/../vendor/autoload.php');
 
-	$params = array();
-	$params['client_id']            = '88473928796-na201krpoji4iacnr7mv90gu72jsl2r1.apps.googleusercontent.com';
-	$params['service_account_name'] = '88473928796-na201krpoji4iacnr7mv90gu72jsl2r1@developer.gserviceaccount.com';
-	$params['key']                  = __DIR__.'/../Drive-270b497d578f.p12';
-	$params['sub']                  = 'jacob@mathisonprojects.com';
-	$params['drive']				= '0BwAvSETHEv4nfk1ua0gwVXJDNllvRnNlSDlIdEZYN1hobTVXX3ZoU1BFREV0VHdFVFJGNjQ';
+    $params = array();
+    $params['client_id']            = 'xyz.apps.googleusercontent.com';
+    $params['service_account_name'] = 'xyz@developer.gserviceaccount.com';
+    $params['key']                  = 'your_directory.P12';
+    $params['sub']                  = 'youremail@example.com';
+    $params['drive']                = 'drive_folder';
 
-	$data = new \Divinityfound\GoogleDriveTree\Processor($params);
+    $data = new \Divinityfound\GoogleDriveTree\Processor($params);
 
-	echo '<pre>';
-	print_r($data->file_tree);
-	echo '</pre>';
-	exit;
+    echo '<pre>';
+    print_r($data->file_tree);
+    echo '</pre>';
+    exit;
 ?>
